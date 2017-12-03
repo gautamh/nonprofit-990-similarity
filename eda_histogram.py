7f1585ce1afa03fa3fa88a4cc7c0085957cff1e1
Gautam Hathi <gautamhathi@live.com>
2017-12-02 18:24:58 -0800

    code to get histograms of nicely transformed non-correlated features

---
+++ eda_histogram.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# read in data
+df = pd.read_csv('C:/Users/Gautam/nonprofit_features.csv')
+df = df.rename(columns=lambda x: str(x).replace('{http://www.irs.gov/efile}', ''))
+
+# drop highly correlated columns
+corr_matrix = df.corr().abs()
+upper = corr_matrix.where(np.triu(np.ones(corr_matrix.shape), k=1).astype(np.bool))
+to_drop = [column for column in upper.columns if any(upper[column] > 0.80)]
+df2 = df.drop(to_drop, axis=1)
+
+# shift columns so that all data is positive (for log transformation)
+mins = [(col, df2[[col]].min()) for col in df2.columns]
+for col, min_val in mins:
+    if min_val[0] < 1:
+        df2[[col]] = df2[[col]].apply(lambda x: x - min_val[0] + 1)
+
+LOG_COLUMNS = [u'GrossReceiptsAmt', u'VotingMembersGoverningBodyCnt', u'VotingMembersIndependentCnt', u'TotalEmployeeCnt', u'CYContributionsGrantsAmt', u'CYGrantsAndSimilarPaidAmt', u'CYBenefitsPaidToMembersAmt', u'CYSalariesCompEmpBnftPaidAmt', u'CYTotalFundraisingExpenseAmt', u'IRPDocumentCnt', u'IRPDocumentW2GCnt', u'CYProgramServiceRevenueAmt', u'CYTotalProfFndrsngExpnsAmt', u'TotalAssetsEOYAmt', u'TotalGrossUBIAmt']
+
+df2[LOG_COLUMNS] = df2[LOG_COLUMNS].apply(lambda x: np.log(x))
+
+df2.hist(layout=(3,6), bins=50)
+
+plt.show()
