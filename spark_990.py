3375b771c52dbfd523e88dca15362002d6c21849
Gautam Hathi <gautamhathi@live.com>
2017-12-04 22:51:19 -0800

    modifying to extract numerical fields from 990s, but doesn't matter since spark didn't work and this should be scrapped at some point

--- spark_990.py
+++ spark_990.py
@@ -9,19 +9,15 @@ from urllib.error import URLError
 sc = SparkContext()
 
 index_file = "/mnt/c/Users/Gautam/Downloads/index_2017.csv"
+filings_directory = "/mnt/c/Users/Gautam/form_990"
 
-def process_filing_link(filing_url):
+def get_numfields_from_filing_link(filing_url):
     num_fields = []
     filing = ""
     try:
         with urlopen(filing_url) as resp:
             filing = resp.read()
-        root = etree.fromstring(filing)
-        form_990 = root.find("./{http://www.irs.gov/efile}ReturnData/{http://www.irs.gov/efile}IRS990")
-        if (form_990 is not None and len(form_990) > 0):
-            for child in form_990:
-                if child.text is not None and child.text.isdigit():
-                    num_fields.append((child.tag, 1))
+        num_fields = get_numfields_from_filing(filing)
     except HTTPError as e:
         print(str(e))
         print("500 ERROR: {}".format(filing_url))
@@ -35,25 +31,37 @@ def process_filing_link(filing_url):
         return []
     return num_fields
 
-filings = sc.textFile(index_file)
+def get_numfields_from_filing(filing):
+    num_fields = []
+    root = etree.fromstring(filing)
+    form_990 = root.find("./{http://www.irs.gov/efile}ReturnData/{http://www.irs.gov/efile}IRS990")
+    if (form_990 is not None and len(form_990) > 0):
+        for child in form_990:
+            if child.text is not None and child.text.isdigit():
+                num_fields.append((child.tag, 1))
+    return num_fields
+
+#filings = sc.textFile(index_file)
+filings = sc.wholeTextFiles(filings_directory).map(lambda x: x[1])
 
 #split lines
-print("splitting lines")
-rows = filings.map(lambda line: line.split(','))
+#print("splitting lines")
+#rows = filings.map(lambda line: line.split(','))
 
 #extract header
-print("getting header")
-header = rows.first()
-print("filtering header")
-rows = rows.filter(lambda row: row != header) 
+#print("getting header")
+#header = rows.first()
+#print("filtering header")
+#rows = rows.filter(lambda row: row != header) 
 
 #get links
-print("getting links")
-filing_links = rows.map(lambda row: "https://s3.amazonaws.com/irs-form-990/{}_public.xml".format(row[8]))
+#print("getting links")
+#filing_links = rows.map(lambda row: "https://s3.amazonaws.com/irs-form-990/{}_public.xml".format(row[8]))
 
 #get fields
 print("getting fields")
-fields = filing_links.flatMap(lambda link: process_filing_link(link))
+#fields = filing_links.flatMap(lambda link: get_numfields_from_filing_link(link))
+fields = filings.flatMap(lambda filing: get_numfields_from_filing(filing))
 
 #sum field counts
 print(sorted(fields.reduceByKey(add).collect(), key = lambda x: -x[1]))
