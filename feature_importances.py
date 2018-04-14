95b46e3c8002d10ee552b3a5d29fe3e6f8481578
Gautam Hathi <gautamhathi@live.com>
2018-04-14 15:31:07 -0700

    removing Z00 from categories

--- feature_importances.py
+++ feature_importances.py
@@ -5,7 +5,9 @@ from sklearn.cross_validation import cross_val_score
 
 import common_utils
 
-df2 = common_utils.load_data('C:/Users/Gautam/nonprofit_features_ntee.csv')
+df = common_utils.load_data('C:/Users/Gautam/nonprofit_features_ntee.csv')
+
+df2 = df.loc[df['NTEE_CD'] != 'Z00']
 
 feature_cols = [u'GrossReceiptsAmt', u'VotingMembersGoverningBodyCnt', u'VotingMembersIndependentCnt', u'TotalEmployeeCnt', u'CYContributionsGrantsAmt', u'CYGrantsAndSimilarPaidAmt', u'CYBenefitsPaidToMembersAmt', u'CYSalariesCompEmpBnftPaidAmt', u'CYTotalFundraisingExpenseAmt', u'IRPDocumentCnt', u'IRPDocumentW2GCnt', u'CYProgramServiceRevenueAmt', u'CYTotalProfFndrsngExpnsAmt', u'TotalAssetsEOYAmt', u'TotalGrossUBIAmt', u'CYOtherRevenueAmt', u'FormationYr']
 
@@ -21,5 +23,5 @@ rf.fit(features, labels)
 
 weights = rf.feature_importances_
 
-print(common_utils.nearest_neighbor(df2, 'EIN', 560532129, ['EIN', 'NTEE_CD'], weights))
-print(common_utils.nearest_neighbor(df2, 'EIN', 560532129, ['EIN', 'NTEE_CD']))
+print(common_utils.nearest_neighbor(df, 'EIN', 133871360, ['EIN', 'NTEE_CD'], weights))
+print(common_utils.nearest_neighbor(df, 'EIN', 133871360, ['EIN', 'NTEE_CD']))
